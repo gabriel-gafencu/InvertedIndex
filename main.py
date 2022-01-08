5f3e494e117aa41d56a08b8b2b851e2feb41c383
Cojocaru Cosmin <cosmin29@cosmin29.kali-home.com>
2022-01-07 22:44:01 -0500

    Adding test cases

--- main.py
+++ main.py
@@ -2,16 +2,40 @@ from mpi4py import MPI
 import sys
 import re
 import json
+import os
 
 BEGIN = 0
 END = 1
 
-master = 0
-mappers = [1, 2, 3, 4, 5]
-reducers = [6, 7, 8]
+N_RESERVED_RANKS = 4
 
 comm = MPI.COMM_WORLD
+size = comm.Get_size()
 rank = comm.Get_rank()
+
+master = 0
+mappers = [] #[1, 2, 3, 4, 5]
+reducers = [1, 2, 3]  #[6, 7, 8]
+
+if len(sys.argv) != 2:
+    if size <= N_RESERVED_RANKS:
+        raise Exception("Number of processes must be greater than 4.")
+    else:
+        mappers = [x for x in range(N_RESERVED_RANKS, size)]
+else:
+    raise Exception("Program taken {} arguments but {} provided.\nExample: python <input-dir> <output-dir>\n".format(2, len(sys.argv)))
+
+INPUT_DIR = sys.argv[1]
+BASE_DIR = sys.argv[2]
+MAPPERS_DIR = BASE_DIR+"/map"
+REDUCERS_DIR = BASE_DIR+"/reduce"
+
+# if rank == 0:
+#     pass
+# else:
+#     sys.exit(0)
+
+
 
 '''
 Deoarece algoritmul de calcul a Inverted Index este bazat pe algoritmul map-reduce, care se foloseste de paradigma 
@@ -127,8 +151,30 @@ Se demonstreaza corectitudinea pentru secventa de reducere a fiecarui proces.
 
 
 '''
+
+
 if rank == master:
     # Master delegates tasks according to rank and orchestrates the map-reduce process
+
+    # Printing status for curent execution
+    print("\n\nProgram started")
+    print("Master rank: {}".format(master))
+    print("Mappers: {}".format(mappers))
+    print("Reducers: {}".format(reducers))
+    print("Input directory: {}".format(INPUT_DIR))
+    print("Output directory: {}\n".format(BASE_DIR))
+
+
+    # Creating directories for storing the results
+    
+    if os.path.exists(BASE_DIR) == False:
+        os.mkdir(BASE_DIR)
+    
+    if os.path.exists(MAPPERS_DIR) == False:
+        os.mkdir(MAPPERS_DIR)
+
+    if os.path.exists(REDUCERS_DIR) == False:
+        os.mkdir(REDUCERS_DIR)
 
     # tell mapper processes to begin
     for mapper in mappers:
@@ -160,27 +206,45 @@ elif rank in mappers:
 
     # each mapper is responsible for 5 files:
     # process 1 for 1-5, process 2 for 6-10, ect.
-    my_files = [x for x in range((rank - 1) * 5 + 1, rank * 5 + 1)]
-    for file_number in my_files:
-        file_name = '{}.txt'.format(file_number)
-        with open('test-files/{}'.format(file_name), 'r') as f:
-            while True:
-                try:
-                    line = next(f)
-                    words = line.split()
-                    for word in words:
-                        # make all words lower case as searching for terms is case insensitive
-                        word = re.sub('[^a-zA-Z]+', '', word.lower())
-                        if len(word) > 0:
-                            file = open('intermediary/map/{}.txt'.format(word[0]), 'a')
-                            file.write('{} {}\n'.format(word, file_name))
-                            file.close()
-                except StopIteration:
-                    break
-                except UnicodeError:
-                    # some of the files might raise decoding errors
-                    # in this case simply ignore the line and move on to the next one
-                    pass
+
+    N_MAPPERS = size - N_RESERVED_RANKS
+    MAPPER_INDEX = rank - N_RESERVED_RANKS
+
+    all_files = [file for file in os.listdir(INPUT_DIR)]
+    files_per_process = int(len(all_files) / N_MAPPERS)
+
+    my_files = [all_files[file_index] for file_index in range(MAPPER_INDEX * files_per_process, (MAPPER_INDEX + 1) * files_per_process)]
+    
+    for file_index in range(N_MAPPERS * files_per_process, len(all_files)):
+        if file_index % files_per_process == MAPPER_INDEX:
+            my_files.append(all_files[file_index]) 
+
+    print("Process rank {} start to process files: {}".format(rank, my_files))
+
+    for file_name in my_files:
+        try:
+            with open('{}/{}'.format(INPUT_DIR, file_name), 'r') as f:
+                while True:
+                    try:
+                        line = next(f)
+                        words = line.split()
+                        for word in words:
+                            # make all words lower case as searching for terms is case insensitive
+                            word = re.sub('[^a-zA-Z]+', '', word.lower())
+                            if len(word) > 0:
+                                file = open('{}/{}.txt'.format(MAPPERS_DIR, word[0]), 'a')
+                                file.write('{} {}\n'.format(word, file_name))
+                                file.close()
+                    except StopIteration:
+                        break
+                    except UnicodeError:
+                        # some of the files might raise decoding errors
+                        # in this case simply ignore the line and move on to the next one
+                        pass
+        except FileNotFoundError:
+            # this happends when some words miss becouse of lack of text
+            # we simply continue
+            pass
 
     comm.send('end', dest=master, tag=END)
 
@@ -208,37 +272,42 @@ elif rank in reducers:
             'stop': 'z'
         }
     }
+
     reducer_rank = str(rank - reducers[0])
-    my_files = ['{}.txt'.format(chr(x)) for x in
-                range(ord(limiters[reducer_rank]['start']), ord(limiters[reducer_rank]['stop']) + 1)]
-    end_file = 'intermediary/reduce/{}-{}.txt'.format(limiters[reducer_rank]['start'], limiters[reducer_rank]['stop'])
+    my_files = ['{}.txt'.format(chr(x)) for x in range(ord(limiters[reducer_rank]['start']), ord(limiters[reducer_rank]['stop']) + 1)]
+    end_file = '{}/{}-{}.txt'.format(REDUCERS_DIR,limiters[reducer_rank]['start'], limiters[reducer_rank]['stop'])
 
     #de aici demonstrez corectitudinea pentru reducer
     for file in my_files:
         terms = {}
-        with open('intermediary/map/{}'.format(file), 'r') as f:
-            while True:
-                try:
-                    line = next(f).split()
-                    if len(line) == 2:
-                        word = line[0]
-                        original_file = line[1]
+        try:
+            with open('{}/{}'.format(MAPPERS_DIR, file), 'r') as f:
+                while True:
+                    try:
+                        line = next(f).split()
+                        if len(line) == 2:
+                            word = line[0]
+                            original_file = line[1]
 
-                        if word in terms:
-                            if original_file in terms[word]:
-                                terms[word][original_file] += 1
+                            if word in terms:
+                                if original_file in terms[word]:
+                                    terms[word][original_file] += 1
+                                else:
+                                    terms[word][original_file] = 1
                             else:
+                                terms[word] = {}
                                 terms[word][original_file] = 1
-                        else:
-                            terms[word] = {}
-                            terms[word][original_file] = 1
 
-                except StopIteration:
-                    break
-                except UnicodeError:
-                    # some of the files might raise decoding errors
-                    # in this case simply ignore the line and move on to the next one
-                    pass
+                    except StopIteration:
+                        break
+                    except UnicodeError:
+                        # some of the files might raise decoding errors
+                        # in this case simply ignore the line and move on to the next one
+                        pass
+        except FileNotFoundError:
+            # this happends when some words miss becouse of lack of text
+            # we simply continue
+            pass
 
         # sort terms dict by terms
         terms = dict(sorted(terms.items()))
@@ -248,7 +317,7 @@ elif rank in reducers:
 
         pretty_terms = json.dumps(terms, indent=4)
         write_file = open(end_file, 'a')
-        write_file.write('{}\n'.format(pretty_terms))
+        write_file.write('{}\n~\n'.format(pretty_terms))
         write_file.close()
 
     comm.send('end', dest=master, tag=END)
---
+++ test-cases/test_cases.py
@@ -0,0 +1,95 @@
+import unittest
+import json
+import os
+import shutil
+
+class InvertedIndexTestClass(unittest.TestCase):
+
+    def __init__(self, methodName: str = ...) -> None:
+        super().__init__(methodName=methodName)
+
+        PATH = os.getcwd()
+        
+        self.INPUT_BASE_DIR = PATH+"/input_files/"
+        self.INPUT_DIR_1 = self.INPUT_BASE_DIR+"input_files_1"
+        self.INPUT_DIR_2 = self.INPUT_BASE_DIR+"input_files_2"
+        self.INPUT_DIR_3 = self.INPUT_BASE_DIR+"input_files_3"
+        self.INPUT_DIR_4 = self.INPUT_BASE_DIR+"input_files_4"
+        self.INPUT_DIR_5 = self.INPUT_BASE_DIR+"input_files_5"
+        
+        self.OUTPUT_BASE_DIR = PATH+"/output_files/"
+        self.OUTPUT_DIR_1 = self.OUTPUT_BASE_DIR+"output_files_1"
+        self.OUTPUT_DIR_2 = self.OUTPUT_BASE_DIR+"output_files_2"
+        self.OUTPUT_DIR_3 = self.OUTPUT_BASE_DIR+"output_files_3"
+        self.OUTPUT_DIR_4 = self.OUTPUT_BASE_DIR+"output_files_4"
+        self.OUTPUT_DIR_5 = self.OUTPUT_BASE_DIR+"output_files_5"
+
+    def data_to_dictionary(self, data):
+        list_of_jsons = [js for js in data.split('~')]
+        list_of_jsons = list_of_jsons[0:len(list_of_jsons)-1]
+        list_of_jsons = [json.loads(js) for js in list_of_jsons]
+
+        result_json = {}
+        for dic in list_of_jsons:
+            for key in dic:
+                result_json[key] = dic[key]
+        return result_json
+
+    def setUp(self):
+        if os.path.exists(self.OUTPUT_BASE_DIR) == False:
+            os.mkdir(self.OUTPUT_BASE_DIR)
+
+    def test_reducer_input_files_1_a_f_txt(self):
+        os.system("mpiexec --hostfile ../hostfile -n {} python3 ../main.py {} {}".format(5, self.INPUT_DIR_1, self.OUTPUT_DIR_1))
+        
+        with open(self.OUTPUT_DIR_1+"/reduce/a-f.txt", 'r') as f:
+            output_data = f.read()
+
+        json_data = self.data_to_dictionary(output_data)
+
+        shouldBeTrue = True
+        shouldBeTrue &= json_data['a']['input1.txt'] == 1
+        shouldBeTrue &= json_data['a']['input2.txt'] == 1
+        shouldBeTrue &= json_data['a']['input3.txt'] == 1
+        shouldBeTrue &= json_data['a']['input4.txt'] == 1
+        shouldBeTrue &= json_data['a']['input5.txt'] == 1
+
+        self.assertTrue(shouldBeTrue)
+
+    def test_reducer_input_files_1_g_p_txt(self):
+        os.system("mpiexec --hostfile ../hostfile -n {} python3 ../main.py {} {}".format(5, self.INPUT_DIR_1, self.OUTPUT_DIR_1))
+        
+        with open(self.OUTPUT_DIR_1+"/reduce/g-p.txt", 'r') as f:
+            output_data = f.read()
+
+        json_data = self.data_to_dictionary(output_data)
+
+        shouldBeTrue = True
+        shouldBeTrue &= json_data['g']['input1.txt'] == 1
+        shouldBeTrue &= json_data['g']['input2.txt'] == 1
+        shouldBeTrue &= json_data['g']['input3.txt'] == 1
+        shouldBeTrue &= json_data['g']['input4.txt'] == 1
+        shouldBeTrue &= json_data['g']['input5.txt'] == 1
+
+        self.assertTrue(shouldBeTrue)
+
+    def test_reducer_input_files_2(self):
+        os.system("mpiexec --hostfile ../hostfile -n {} python3 ../main.py {} {}".format(5, self.INPUT_DIR_2, self.OUTPUT_DIR_2))
+        
+        with open(self.OUTPUT_DIR_2+"/reduce/a-f.txt", 'r') as f:
+            output_data = f.read()
+
+        json_data = self.data_to_dictionary(output_data)
+
+        shouldBeTrue = True
+        shouldBeTrue &= json_data['care']['input1.txt'] == 1
+        shouldBeTrue &= json_data['fi']['input1.txt'] == 1
+        shouldBeTrue &= json_data['ep']['input2.txt'] == 1
+
+        self.assertTrue(shouldBeTrue)
+
+    def tearDown(self):
+        try:
+            shutil.rmtree(self.OUTPUT_BASE_DIR)
+        except FileNotFoundError:
+            pass
